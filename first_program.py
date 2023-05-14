8680713417e4d6099a8a5f1d754270e87e09dbf5
Jussten Craddock <Jusstencraddockjc@gmail.com>
2023-05-14 01:10:50 -0500

    Made functions to add and multiply numbers

--- first_program.py
+++ first_program.py
@@ -1,8 +1,12 @@
 #create an input to ask for First and Last Name and age of a person
-first_name = input("Please enter your first name: ")
-last_name = input("Please enter your last name: ")
-age = int(input("Please enter your age: "))
 
-print(first_name, " ", type(first_name)) #Prints first name and states what type it is
-print(last_name, " ", type(last_name))
-print(age, " ", type(age))
+
+def add_numbers(x,y):
+    return x+y
+
+def multiply_number(x):
+    return x*2
+
+if __name__ == "__main__":
+    numbers_added = add_numbers(y=3, x=2)
+    print(multiply_number(numbers_added))
