135a66fbe5463a5ae98814691275a057881c0a7d
Jussten Craddock <Jusstencraddockjc@gmail.com>
2023-05-14 00:52:27 -0500

    Changing names of file and variable names

--- FirstProgram.py
+++
@@ -1,8 +0,0 @@
-#create an input to ask for First and Last Name and age of a person
-firstName = input("Please enter your first name: ")
-lastName = input("Please enter your last name: ")
-age = int(input("Please enter your age: "))
-
-print(firstName, " ", type(firstName)) #Prints first name and states what type it is
-print(lastName, " ", type(lastName))
-print(age, " ", type(age))
---
+++ first_program.py
@@ -0,0 +1,8 @@
+#create an input to ask for First and Last Name and age of a person
+first_name = input("Please enter your first name: ")
+last_name = input("Please enter your last name: ")
+age = int(input("Please enter your age: "))
+
+print(first_name, " ", type(first_name)) #Prints first name and states what type it is
+print(last_name, " ", type(last_name))
+print(age, " ", type(age))
